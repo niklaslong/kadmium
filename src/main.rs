29e1749a4ca7de05471f6784811d0ea035bfef75
Niklas <niklas@long.ch>
2022-07-13 14:11:11 +0200

    feat: working initial impl

--- src/main.rs
+++ src/main.rs
@@ -23,127 +23,129 @@
 //    repeats steps (1)–(3), until an iteration does not yield any nodes closer than the already
 //    known ones anymore.
 
-#![feature(int_log)]
+fn main() {}
 
-use std::{collections::HashMap, net::SocketAddr};
+// #![feature(int_log)]
+//
+// use std::{collections::HashMap, net::SocketAddr};
+//
+// use kadcast::tree::RoutingTable;
+// use pea2pea::{
+//     protocols::{Reading, Writing},
+//     Node as PNode, Pea2Pea,
+// };
+// use rand::{seq::SliceRandom, thread_rng};
+// use time::OffsetDateTime;
+//
+// fn main() {
+//     let a: u128 = 3;
+//     let b: u128 = 2;
+//
+//     println!("{}", a ^ b);
+//     // println!("{}", (a ^ b).log2());
+// }
+//
+// // TODO: Nonce and Id should be at least 160 bits.
+// type Nonce = u128;
+// type Id = u128;
+//
+// const K: u8 = 20;
+// const A: u8 = 3;
+//
+// struct PingPayload {
+//     nonce: Nonce,
+//     // The sending node's ID.
+//     id: Id,
+//     // The sending node's addr.
+//     addr: SocketAddr,
+// }
+//
+// struct PongPayload {
+//     // The nonce sent in the corresponding Ping.
+//     nonce: Nonce,
+//     // The sending node's ID.
+//     id: Id,
+// }
+//
+// struct FindNodePayload {
+//     nonce: Nonce,
+//     id: Id,
+// }
+//
+// struct ChunkPayload {
+//     nonce: Nonce,
+//     h: u8,
+// }
+//
+// enum Message {
+//     // Node liveness.
+//     Ping(PingPayload),
+//     Pong(PongPayload),
+//     // Node lookup.
+//     FindNode(FindNodePayload),
+//     // Propagation.
+//     Chunk(ChunkPayload),
+// }
+//
+// struct Node {
+//     pnode: PNode,
+//     routing_table: RoutingTable,
+// }
 
-use kadcast::tree::RoutingTable;
-use pea2pea::{
-    protocols::{Reading, Writing},
-    Node as PNode, Pea2Pea,
-};
-use rand::{seq::SliceRandom, thread_rng};
-use time::OffsetDateTime;
+// impl Node {
+//     fn process_ping(&mut self, payload: PingPayload) {
+//         // Insert the peer's routing information into the table.
+//         self.routing_table.insert(payload.id, payload.addr);
+//
+//         // TODO: respond with PONG.
+//     }
+//
+//     fn process_pong(&mut self, payload: PongPayload) {
+//         // Update the bucket order, the index of the bucket in the list is the XOR distance.
+//         self.routing_table
+//             .update_last_seen(payload.id, OffsetDateTime::now_utc())
+//     }
+//
+//     fn process_find_node(&self, payload: FindNodePayload) {
+//         self.routing_table.find_k_closest(payload.id);
+//
+//         // TODO: respond with KNODES.
+//     }
+//
+//     fn process_k_nodes() {}
+//
+//     fn process_chunk(&self, payload: ChunkPayload) {
+//         // This is where the buckets come in handy. When a node processes a chunk message, it
+//         // selects peers in buckets ]h, 0] and propagates the CHUNK message. If h = 0, no
+//         // propagation occurs.
+//
+//         let mut rng = thread_rng();
+//
+//         // TODO: verify received data, if it is malicious don't propagate further to avoid DoS.
+//
+//         if payload.h == 0 {
+//             return;
+//         }
+//
+//         let mut next_addrs = vec![];
+//         for h in 0..payload.h {
+//             // Select a peer at random in each bucket.
+//             // TODO: prioritise connected peers, make a connection if absolutely necessary but this
+//             // makes the broadcast less reliable.
+//             if let Some(next_id) = self.routing_table.buckets[h as usize].choose(&mut rng) {
+//                 next_addrs.push(self.routing_table.peer_meta.get(next_id));
+//             }
+//         }
+//
+//         // TODO: propagate the CHUNK message to each next_addr.
+//     }
+// }
 
-fn main() {
-    let a: u128 = 3;
-    let b: u128 = 2;
-
-    println!("{}", a ^ b);
-    // println!("{}", (a ^ b).log2());
-}
-
-// TODO: Nonce and Id should be at least 160 bits.
-type Nonce = u128;
-type Id = u128;
-
-const K: u8 = 20;
-const A: u8 = 3;
-
-struct PingPayload {
-    nonce: Nonce,
-    // The sending node's ID.
-    id: Id,
-    // The sending node's addr.
-    addr: SocketAddr,
-}
-
-struct PongPayload {
-    // The nonce sent in the corresponding Ping.
-    nonce: Nonce,
-    // The sending node's ID.
-    id: Id,
-}
-
-struct FindNodePayload {
-    nonce: Nonce,
-    id: Id,
-}
-
-struct ChunkPayload {
-    nonce: Nonce,
-    h: u8,
-}
-
-enum Message {
-    // Node liveness.
-    Ping(PingPayload),
-    Pong(PongPayload),
-    // Node lookup.
-    FindNode(FindNodePayload),
-    // Propagation.
-    Chunk(ChunkPayload),
-}
-
-struct Node {
-    pnode: PNode,
-    routing_table: RoutingTable,
-}
-
-impl Node {
-    fn process_ping(&mut self, payload: PingPayload) {
-        // Insert the peer's routing information into the table.
-        self.routing_table.insert(payload.id, payload.addr);
-
-        // TODO: respond with PONG.
-    }
-
-    fn process_pong(&mut self, payload: PongPayload) {
-        // Update the bucket order, the index of the bucket in the list is the XOR distance.
-        self.routing_table
-            .update_last_seen(payload.id, OffsetDateTime::now_utc())
-    }
-
-    fn process_find_node(&self, payload: FindNodePayload) {
-        self.routing_table.find_k_closest(payload.id);
-
-        // TODO: respond with KNODES.
-    }
-
-    fn process_k_nodes() {}
-
-    fn process_chunk(&self, payload: ChunkPayload) {
-        // This is where the buckets come in handy. When a node processes a chunk message, it
-        // selects peers in buckets ]h, 0] and propagates the CHUNK message. If h = 0, no
-        // propagation occurs.
-
-        let mut rng = thread_rng();
-
-        // TODO: verify received data, if it is malicious don't propagate further to avoid DoS.
-
-        if payload.h == 0 {
-            return;
-        }
-
-        let mut next_addrs = vec![];
-        for h in 0..payload.h {
-            // Select a peer at random in each bucket.
-            // TODO: prioritise connected peers, make a connection if absolutely necessary but this
-            // makes the broadcast less reliable.
-            if let Some(next_id) = self.routing_table.buckets[h as usize].choose(&mut rng) {
-                next_addrs.push(self.routing_table.peer_meta.get(next_id));
-            }
-        }
-
-        // TODO: propagate the CHUNK message to each next_addr.
-    }
-}
-
-impl Pea2Pea for Node {
-    fn node(&self) -> &PNode {
-        &self.pnode
-    }
-}
+// impl Pea2Pea for Node {
+//     fn node(&self) -> &PNode {
+//         &self.pnode
+//     }
+// }
 
 // impl Reading for Node {}
 //
--- src/tree.rs
+++ src/tree.rs
@@ -1,4 +1,8 @@
-use std::{cmp::Ordering, collections::HashMap, net::SocketAddr};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+};
 
 use time::OffsetDateTime;
 
@@ -7,9 +11,9 @@ const K: u8 = 20;
 type Id = u128;
 
 #[derive(Debug, Clone, Copy)]
-enum ConnState {
+pub enum ConnState {
     Connected,
-    Disconneted,
+    Disconnected,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -29,20 +33,14 @@ impl PeerMeta {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum EntryState {
-    Exists,
-    Inserted,
-    Pending,
-    SelfEntry,
-}
-
 pub struct RoutingTable {
+    // The node's local ID.
     pub local_id: Id,
-    pub buckets: Vec<Vec<Id>>,
-    // We could make the cache larger than 1 item per bucket in future, e.g. with a circular queue.
-    pub pending: HashMap<u32, Id>,
-    pub peer_meta: HashMap<Id, PeerMeta>,
+    pub max_bucket_size: u8,
+    // The buckets for broadcast purposes.
+    pub buckets: HashMap<u32, HashSet<Id>>,
+    // Contains the connected and disconnected peer information.
+    pub peer_list: HashMap<Id, PeerMeta>,
 }
 
 impl Default for RoutingTable {
@@ -50,16 +48,26 @@ impl Default for RoutingTable {
         Self {
             // TODO: generate a random u128.
             local_id: 0u128,
-            buckets: vec![vec![]; 128],
-            pending: HashMap::new(),
-            peer_meta: HashMap::new(),
+            max_bucket_size: K,
+            buckets: HashMap::new(),
+            // pending: HashMap::new(),
+            peer_list: HashMap::new(),
         }
     }
 }
 
 impl RoutingTable {
-    // TODO: perhaps we need an enum return type with Inserted, Pending, and SelfEntry.
-    pub fn insert(&mut self, id: Id, peer_meta: PeerMeta) -> EntryState {
+    pub fn new(local_id: Id, max_bucket_size: u8) -> Self {
+        Self {
+            local_id,
+            max_bucket_size,
+            ..Default::default()
+        }
+    }
+
+    // Returns true if the record exists already, false if an attempt was made to insert our local
+    // ID.
+    pub fn insert(&mut self, id: Id, addr: SocketAddr) -> bool {
         // Buckets should only contain connected peers. The other structures should track
         // connection state.
 
@@ -73,87 +81,112 @@ impl RoutingTable {
         // Solution: insert all addresses as disconnected initially, returning whether the relevant
         // bucket would have space. The caller can then use this information to determine whether
         // to initiate a connection in case 1, or accept the connection in case 2.
+        //
+        // Eviction logic (a little different to the standard kadcast protocol):
+        //
+        // 1. nodes are evicted when they disconnect
+        // 2. nodes are evicted periodically based on network latency
 
+        if id == self.local_id {
+            return false;
+        }
+
+        // // Calculate the distance by XORing the ids.
+        // let distance = id ^ self.local_id;
+
+        // // Don't calculate the log if distance is 0, this should only happen if the ID we got from
+        // // the peer is the same as ours.
+        // if distance == u128::MIN {
+        //     return false;
+        // }
+
+        // Insert the peer into the set, if it doesn't exist.
+        self.peer_list.entry(id).or_insert_with(|| {
+            PeerMeta::new(
+                addr,
+                // TODO: this isn't correct as nodes we haven't connected to haven't been "seen".
+                OffsetDateTime::now_utc(),
+                ConnState::Disconnected,
+            )
+        });
+
+        true
+    }
+
+    // Returns if there is space in the particular bucket for that ID and the appropriate bucket
+    // index if there is.
+    pub fn can_connect(&mut self, id: Id) -> (bool, Option<u32>) {
         // Calculate the distance by XORing the ids.
         let distance = id ^ self.local_id;
 
         // Don't calculate the log if distance is 0, this should only happen if the ID we got from
         // the peer is the same as ours.
         if distance == u128::MIN {
-            return EntryState::SelfEntry;
+            return (false, None);
         }
 
         // Calculate the index of the bucket from the distance.
         // Nightly feature.
         let i = distance.log2();
 
-        let bucket = self
-            .buckets
-            .get_mut(i as usize)
-            .expect("bucket should exist");
+        let bucket = self.buckets.entry(i).or_insert_with(HashSet::new);
 
-        // This is O(n), perhaps check the hashmap instead?
-        // Q: should an entry in the hashmap be removed when a node is removed from the bucket?
-        if bucket.contains(&id) {
-            return EntryState::Exists;
-        }
-
-        match bucket.len().cmp(&K.into()) {
+        match bucket.len().cmp(&self.max_bucket_size.into()) {
             Ordering::Less => {
-                // Bucket still has space.
-                bucket.push(id);
-                self.peer_meta
-                    .insert(id, PeerMeta::new(addr, OffsetDateTime::now_utc()));
-
-                EntryState::Inserted
+                // Bucket still has space. Signal the value could be inserted into the bucket (once
+                // the connection is succesful).
+                (true, Some(i))
             }
             Ordering::Equal => {
-                // Bucket is full.
-                self.pending.insert(i, id);
-
-                EntryState::Pending
+                // Bucket is full. Signal the value can't currently be inserted into the bucket.
+                (false, None)
             }
             Ordering::Greater => {
                 // Bucket is over capacity, this should never happen.
-                // TODO: consider using an array instead of a vec?
                 unreachable!()
             }
         }
     }
 
-    pub fn sort(&mut self) {
-        // Sort the buckets by last seen, this should be called every time we measure the latency
-        // for a peer (PING messages).
-        self.buckets.iter_mut().for_each(|bucket| {
-            bucket.sort_by(|a, b| {
-                let a_ls = self.peer_meta.get(a).unwrap().last_seen;
-                let b_ls = self.peer_meta.get(b).unwrap().last_seen;
+    pub fn set_connected(&mut self, id: Id) -> bool {
+        match self.can_connect(id) {
+            (true, Some(i)) => {
+                // TODO: if this is true, the id was already in the bucket, this should probably be handled
+                // in some way or another, currently we just update the peer metadata.
+                if let Some(bucket) = self.buckets.get_mut(&i) {
+                    bucket.insert(id);
+                }
 
-                // We want the most recently seen nodes at the head of the Vec.
-                b_ls.partial_cmp(&a_ls).unwrap()
-            })
-        });
+                if let Some(peer_meta) = self.peer_list.get_mut(&id) {
+                    peer_meta.conn_state = ConnState::Connected;
+                }
+
+                true
+            }
+
+            _ => false,
+        }
     }
 
-    pub fn update_last_seen(&mut self, id: Id, last_seen: OffsetDateTime) {
-        if let Some(peer_meta) = self.peer_meta.get_mut(&id) {
+    pub fn set_last_seen(&mut self, id: Id, last_seen: OffsetDateTime) {
+        if let Some(peer_meta) = self.peer_list.get_mut(&id) {
             peer_meta.last_seen = last_seen
         }
     }
 
-    pub fn find_k_closest(&self, id: Id) -> Vec<(Id, PeerMeta)> {
+    pub fn find_k_closest(&self, id: Id, k: usize) -> Vec<(Id, PeerMeta)> {
         // Find the K closest nodes to the given ID. There is a total order over the keyspace, so a
         // sort won't yield any conflicts.
         //
         // Naive way: just iterate over all the IDs and XOR them? Need a map of ID to the addr, to
         // be sent to the requesting node.
         let mut ids: Vec<_> = self
-            .peer_meta
+            .peer_list
             .iter()
             .map(|(&candidate_id, &candidate_meta)| (candidate_id, candidate_meta))
             .collect();
         ids.sort_by_key(|(candidate_id, _)| candidate_id ^ id);
-        ids.truncate(K.into());
+        ids.truncate(k);
 
         ids
     }
@@ -164,91 +197,62 @@ mod tests {
     use super::*;
 
     #[test]
-    fn self_insert() {
-        let mut rt = RoutingTable::default();
+    fn insert() {
+        let mut rt = RoutingTable::new(0, 1);
 
         // Attempt to insert our local id.
-        assert_eq!(
-            rt.insert(rt.local_id, "127.0.0.1:1".parse().unwrap()),
-            EntryState::SelfEntry
-        );
-        // Attempt to insert the next id (in base 10).
-        // TODO: should fail when inserting the same addr.
-        assert_eq!(
-            rt.insert(rt.local_id + 1, "127.0.0.1:1".parse().unwrap()),
-            EntryState::Inserted
-        );
-        // Attempt to insert the same id again.
-        assert_eq!(
-            rt.insert(rt.local_id + 1, "127.0.0.1:1".parse().unwrap()),
-            EntryState::Exists
-        );
+        assert!(!rt.insert(rt.local_id, "127.0.0.1:0".parse().unwrap()));
+
+        // ... 0001 -> bucket i = 0
+        assert!(rt.insert(1, "127.0.0.1:1".parse().unwrap()));
+        // ... 0010 -> bucket i = 1
+        assert!(rt.insert(2, "127.0.0.1:2".parse().unwrap()));
+        // ... 0011 -> bucket i = 1
+        // This should still return true, since no peers have been inserted into the buckets yet
+        // and there is still space.
+        assert!(rt.insert(3, "127.0.0.1:3".parse().unwrap()));
     }
 
     #[test]
-    fn last_seen_update() {
-        let mut rt = RoutingTable::default();
-        let peer_id = rt.local_id + 1;
-
-        // Insert a peer ID.
-        assert_eq!(
-            rt.insert(peer_id, "127.0.0.1:1".parse().unwrap()),
-            EntryState::Inserted
-        );
-        let initial_last_seen = rt.peer_meta.get(&peer_id).unwrap().last_seen;
-        let expected_final_last_seen = OffsetDateTime::now_utc();
-
-        // Update the timestamp.
-        rt.update_last_seen(peer_id, expected_final_last_seen);
-
-        let final_last_seen = rt.peer_meta.get(&peer_id).unwrap().last_seen;
-
-        // Check the timestamp has been updated.
-        assert_eq!(final_last_seen, expected_final_last_seen);
-        assert!(final_last_seen > initial_last_seen);
-    }
-
-    #[test]
-    fn bucket_sort() {
-        let mut rt = RoutingTable::default();
+    fn set_connected() {
+        // Set the max bucket size to a low value so we can easily test when it's full.
+        let mut rt = RoutingTable::new(0, 1);
 
         // ... 0001 -> bucket i = 0
         rt.insert(1, "127.0.0.1:1".parse().unwrap());
+        assert!(rt.set_connected(1));
         // ... 0010 -> bucket i = 1
         rt.insert(2, "127.0.0.1:2".parse().unwrap());
+        assert!(rt.set_connected(2));
         // ... 0011 -> bucket i = 1
         rt.insert(3, "127.0.0.1:3".parse().unwrap());
-        // ... 0010 -> bucket i = 2
-        rt.insert(4, "127.0.0.1:4".parse().unwrap());
-        // ... 0011 -> bucket i = 2
-        rt.insert(5, "127.0.0.1:5".parse().unwrap());
-
-        assert_eq!(rt.buckets[0], vec![1]);
-        assert_eq!(rt.buckets[1], vec![2, 3]);
-        assert_eq!(rt.buckets[2], vec![4, 5]);
-
-        rt.sort();
-
-        assert_eq!(rt.buckets[0], vec![1]);
-        assert_eq!(rt.buckets[1], vec![3, 2]);
-        assert_eq!(rt.buckets[2], vec![5, 4]);
-
-        // TODO: consider calling sort when updating last seen?
-        rt.update_last_seen(2, OffsetDateTime::now_utc());
-
-        assert_eq!(rt.buckets[0], vec![1]);
-        assert_eq!(rt.buckets[1], vec![3, 2]);
-        assert_eq!(rt.buckets[2], vec![5, 4]);
-
-        rt.sort();
-
-        assert_eq!(rt.buckets[0], vec![1]);
-        assert_eq!(rt.buckets[1], vec![2, 3]);
-        assert_eq!(rt.buckets[2], vec![5, 4]);
+        assert!(!rt.set_connected(3));
     }
 
     #[test]
-    fn eviction() {
-        let mut rt = RoutingTable::default();
+    fn find_k_closest() {
+        let mut rt = RoutingTable::new(0, 5);
+
+        // Generate 5 IDs and addressses.
+        let peers: Vec<(Id, SocketAddr)> = (1..=5)
+            .into_iter()
+            .map(|i| (i as u128, format!("127.0.0.1:{}", i).parse().unwrap()))
+            .collect();
+
+        for peer in peers {
+            assert!(rt.insert(peer.0, peer.1));
+            assert!(rt.set_connected(peer.0));
+        }
+
+        let k = 3;
+        let k_closest = rt.find_k_closest(rt.local_id, k);
+
+        assert_eq!(k_closest.len(), 3);
+
+        // The closest IDs are in the same order as the indexes, they are however offset by 1.
+        for (i, (id, peer_meta)) in k_closest.into_iter().enumerate() {
+            assert_eq!(id, (i + 1) as u128);
+            assert_eq!(peer_meta.listening_addr.port(), (i + 1) as u16);
+        }
     }
 }
