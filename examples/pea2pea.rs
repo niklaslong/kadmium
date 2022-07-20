ff0c982093852e73aade41d3f6dfc652a0ddce67
Niklas <niklas@long.ch>
2022-07-20 12:55:56 +0200

    feat: pea2pea example base

--- examples/pea2pea.rs
+++ examples/pea2pea.rs
@@ -1,36 +1,22 @@
-// Messages:
-//
-// - PING: transmits the node's routing information to the recipient.
-// - FIND_NODE:
-//
-// Data structures:
-//
-// - Tree
-// - Bucket
-//
-// Global parameters:
-//
-// - α: number of nodes to query for k closest nodes (distance is d(x, y) = XOR(x, y) in base 10),
-// in practice α = 3.
-// - k: bucket size (upper bound?), in practice k in [20, 100]
-//
-// Lookup procedure:
-//
-// 1. The node looks up the α closest nodes with respect to the XOR-metric in its own buckets.
-// 2. It queries these α nodes for the ID by sending FIND_NODE messages.
-// 3. The queried nodes respond with a set of k nodes they believe to be closest to ID.
-// 4. Based on the acquired information, the node builds a new set of closest nodes and iteratively
-//    repeats steps (1)–(3), until an iteration does not yield any nodes closer than the already
-//    known ones anymore.
+use std::{io, net::SocketAddr, sync::Arc};
 
-use kadcast::tree::RoutingTable;
-use pea2pea::{Node as PNode, Pea2Pea};
+use bytes::BytesMut;
+use kadcast::{message::Message, tree::RoutingTable};
+use parking_lot::RwLock;
+use pea2pea::{
+    protocols::{Reading, Writing},
+    ConnectionSide, Node as PNode, Pea2Pea,
+};
+use tokio_util::codec::{Decoder, Encoder, LengthDelimitedCodec};
+
+type RawData = Vec<u8>;
 
 fn main() {}
 
+#[derive(Clone)]
 struct Node {
     pnode: PNode,
-    routing_table: RoutingTable,
+    routing_table: Arc<RwLock<RoutingTable>>,
 }
 
 impl Node {}
@@ -41,6 +27,71 @@ impl Pea2Pea for Node {
     }
 }
 
-// impl Reading for Node {}
-//
-// impl Writing for Node {}
+#[async_trait::async_trait]
+impl Reading for Node {
+    type Message = Message<RawData>;
+    type Codec = MessageCodec;
+
+    fn codec(&self, _addr: SocketAddr, _side: ConnectionSide) -> Self::Codec {
+        MessageCodec {
+            codec: LengthDelimitedCodec::new(),
+        }
+    }
+
+    async fn process_message(&self, _source: SocketAddr, message: Self::Message) -> io::Result<()> {
+        match message {
+            Message::Ping(ping) => Some(self.routing_table.write().process_ping(ping)),
+            Message::Pong(pong) => {
+                self.routing_table.write().process_pong(pong);
+                None
+            }
+            _ => None,
+        };
+
+        Ok(())
+    }
+}
+
+impl Writing for Node {
+    type Message = Message<RawData>;
+    type Codec = MessageCodec;
+
+    fn codec(&self, _addr: SocketAddr, _side: ConnectionSide) -> Self::Codec {
+        MessageCodec {
+            codec: LengthDelimitedCodec::new(),
+        }
+    }
+}
+
+struct MessageCodec {
+    codec: LengthDelimitedCodec,
+}
+
+impl Decoder for MessageCodec {
+    type Item = Message<RawData>;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        let bytes = match self.codec.decode(src)? {
+            Some(bytes) => bytes,
+            None => return Ok(None),
+        };
+
+        match bincode::decode_from_slice(&bytes, bincode::config::standard()) {
+            Ok((message, _length)) => Ok(Some(message)),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Other, e)),
+        }
+    }
+}
+
+impl Encoder<Message<RawData>> for MessageCodec {
+    type Error = io::Error;
+
+    fn encode(&mut self, message: Message<RawData>, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        if let Err(e) = bincode::encode_into_slice(message, dst, bincode::config::standard()) {
+            return Err(io::Error::new(io::ErrorKind::Other, e));
+        }
+
+        Ok(())
+    }
+}
