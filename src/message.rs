c23e848e2769b9f1446be4a399a8836d085aa581
Niklas <niklas@long.ch>
2022-07-20 16:13:36 +0200

    feat: use `Bytes` for CHUNK data

--- src/message.rs
+++ src/message.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 
 use bincode::{Decode, Encode};
+use bytes::Bytes;
 use time::OffsetDateTime;
 
 use crate::tree::RoutingTable;
@@ -13,15 +14,20 @@ type Height = u32;
 
 pub struct RawData;
 
+pub enum Response {
+    Unicast(Message),
+    Broadcast(Vec<(SocketAddr, Message)>),
+}
+
 #[derive(Encode, Decode)]
-pub enum Message<T> {
+pub enum Message {
     Ping(Ping),
     Pong(Pong),
 
     FindKNodes(FindKNodes),
     KNodes(KNodes),
 
-    Chunk(Chunk<T>),
+    Chunk(Chunk),
 }
 
 #[derive(Encode, Decode)]
@@ -51,29 +57,47 @@ pub struct KNodes {
 }
 
 #[derive(Encode, Decode)]
-pub struct Chunk<T> {
+pub struct Chunk {
     height: Height,
-    data: T,
+    #[bincode(with_serde)]
+    data: Bytes,
 }
 
 impl RoutingTable {
-    //     pub fn process_message(&mut self, message: Message) -> Option<Response> {
-    //         match message {
-    //             Message::Ping(ping) => Unicast(self.process_ping(ping)),
-    //             Message::Pong(pong) => {
-    //                 self.process_pong(pong);
-    //                 None
-    //             }
-    //             Message::FindKNodes(find_k_nodes) => Unicast(self.process_find_k_nodes(find_k_nodes)),
-    //             Message::KNodes(k_nodes) => {
-    //                 self.process_k_nodes(k_nodes);
-    //                 None
-    //             }
-    //             _ => todo!(),
-    //         }
-    //     }
+    pub fn process_message(&mut self, message: Message) -> Option<Response> {
+        match message {
+            Message::Ping(ping) => {
+                let pong = self.process_ping(ping);
+                Some(Response::Unicast(Message::Pong(pong)))
+            }
+            Message::Pong(pong) => {
+                self.process_pong(pong);
+                None
+            }
+            Message::FindKNodes(find_k_nodes) => {
+                let k_nodes = self.process_find_k_nodes(find_k_nodes);
+                Some(Response::Unicast(Message::KNodes(k_nodes)))
+            }
+            Message::KNodes(k_nodes) => {
+                self.process_k_nodes(k_nodes);
+                None
+            }
+            Message::Chunk(chunk) => {
+                if let Some(broadcast) = self.process_chunk(chunk) {
+                    let broadcast = broadcast
+                        .into_iter()
+                        .map(|(addr, message)| (addr, Message::Chunk(message)))
+                        .collect();
 
-    pub fn process_ping(&mut self, ping: Ping) -> Pong {
+                    Some(Response::Broadcast(broadcast))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    fn process_ping(&mut self, ping: Ping) -> Pong {
         // The peer should already exist in the peer list and be present in the bucket, update
         // the last_seen timestamp.
         self.set_last_seen(ping.id, OffsetDateTime::now_utc());
@@ -86,7 +110,7 @@ impl RoutingTable {
         }
     }
 
-    pub fn process_pong(&mut self, pong: Pong) {
+    fn process_pong(&mut self, pong: Pong) {
         // Update the last seen timestamp.
         self.set_last_seen(pong.id, OffsetDateTime::now_utc());
 
@@ -94,7 +118,7 @@ impl RoutingTable {
         // message nonces for latency calculation?
     }
 
-    pub fn process_find_k_nodes(&self, find_k_nodes: FindKNodes) -> KNodes {
+    fn process_find_k_nodes(&self, find_k_nodes: FindKNodes) -> KNodes {
         // TODO: update last seen?
         let k_closest_nodes = self.find_k_closest(find_k_nodes.id, K as usize);
 
@@ -104,7 +128,7 @@ impl RoutingTable {
         }
     }
 
-    pub fn process_k_nodes(&mut self, k_nodes: KNodes) {
+    fn process_k_nodes(&mut self, k_nodes: KNodes) {
         // Save the new peer information.
         for (id, addr) in k_nodes.nodes {
             self.insert(id, addr);
@@ -114,7 +138,7 @@ impl RoutingTable {
         // continual or only when bootstrapping the network?
     }
 
-    pub fn process_chunk<T>(&self, chunk: Chunk<T>) -> Option<Vec<(Height, SocketAddr)>> {
+    fn process_chunk(&self, chunk: Chunk) -> Option<Vec<(SocketAddr, Chunk)>> {
         // TODO: verify data, perhaps accept a function as a parameter, should return true or false
         // for data verification.
         let is_kosher = true;
@@ -127,6 +151,19 @@ impl RoutingTable {
         }
 
         // TODO: return the wrapped data as well as the peers to propagate to.
-        self.select_broadcast_peers(chunk.height)
+        self.select_broadcast_peers(chunk.height).map(|v| {
+            v.iter()
+                .map(|(height, addr)| {
+                    (
+                        *addr,
+                        Chunk {
+                            height: *height,
+                            // Cheap as the backing storage is shared amongst instances.
+                            data: chunk.data.clone(),
+                        },
+                    )
+                })
+                .collect()
+        })
     }
 }
