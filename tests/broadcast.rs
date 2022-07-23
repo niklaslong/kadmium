c56f654a867313952d09e256b999af072a836f71
Niklas <niklas@long.ch>
2022-07-23 19:22:36 +0200

    test: full mesh broadcast

---
+++ tests/broadcast.rs
@@ -0,0 +1,69 @@
+#![cfg(feature = "codec")]
+
+use bytes::Bytes;
+use kadmium::{
+    message::{Chunk, Message},
+    router::Id,
+};
+use pea2pea::{
+    connect_nodes,
+    protocols::{Handshake, Reading, Writing},
+    Topology,
+};
+use rand::{thread_rng, Rng};
+
+mod common;
+#[allow(unused_imports)]
+use crate::common::{enable_tracing, KadNode};
+
+#[tokio::test]
+async fn broadcast_full_mesh() {
+    // enable_tracing();
+
+    const N: usize = 100;
+    let mut rng = thread_rng();
+
+    let mut nodes = Vec::with_capacity(N);
+    for _ in 0..N {
+        let id = Id::new(rng.gen());
+        let node = KadNode::new(id).await;
+        node.enable_handshake().await;
+        node.enable_reading().await;
+        node.enable_writing().await;
+
+        nodes.push(node);
+    }
+
+    assert!(connect_nodes(&nodes, Topology::Mesh).await.is_ok());
+
+    let broadcaster = nodes.pop().unwrap();
+    let peers = broadcaster
+        .routing_table
+        .read()
+        .select_broadcast_peers(u128::BITS)
+        .unwrap();
+
+    let nonce = rng.gen();
+
+    for (height, addr) in peers {
+        let message = Message::Chunk(Chunk {
+            // Can be used to trace the broadcast. If set differently for each peer here, it will
+            // be the same within a propagation sub-tree.
+            nonce,
+            height,
+            data: Bytes::from("Hello world"),
+        });
+
+        assert!(broadcaster.unicast(addr, message).is_ok());
+    }
+
+    // This needs to be longer when the test is run with more nodes.
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    for node in nodes {
+        let received_messages_g = node.received_messages.read();
+
+        assert_eq!(received_messages_g.len(), 1);
+        assert!(received_messages_g.contains_key(&nonce));
+    }
+}
