9d3d3fb4661e53e9686aca22c8af6d5f1de221c3
Niklas <niklas@long.ch>
2022-08-10 14:04:58 +0200

    feat: rough sketch of needed methods on the quic routing table

--- src/quic/mod.rs
+++ src/quic/mod.rs
@@ -1,8 +1,17 @@
 //! Core routing table implementation fine-tuned for QUIC.
 
-use std::net::SocketAddr;
+use std::{
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+};
 
 use time::OffsetDateTime;
+
+use crate::{
+    id::Id,
+    message::{Message, Response},
+    traits::ProcessData,
+};
 
 type ConnId = usize;
 
@@ -40,4 +49,31 @@ pub struct RoutingTable {
     pub(crate) peer_list: HashMap<Id, QuicMeta>,
     // Maps peer connection identifiers (QUIC) to peer identifiers.
     id_list: HashMap<ConnId, Id>,
+}
+
+impl RoutingTable {
+    fn insert(&mut self, id: Id, addr: SocketAddr) -> bool {
+        todo!()
+    }
+
+    fn set_connected(&mut self, id: Id, conn_id: ConnId, stream_state: StreamState) -> bool {
+        todo!()
+    }
+
+    fn set_disconnected(&mut self, conn_id: ConnId) -> bool {
+        todo!()
+    }
+
+    fn select_broadcast_peers(&self, height: u32) -> Option<Vec<(u32, SocketAddr)>> {
+        todo!()
+    }
+
+    fn process_message<S: Clone, T: ProcessData<S>>(
+        &mut self,
+        state: S,
+        message: Message,
+        conn_id: ConnId,
+    ) -> Option<Response> {
+        todo!()
+    }
 }
